7edfa77235d7538ef9c3b28fe99e9a631f2d0b33
Wendel Demétrio <hey.wendel95@gmail.com>
2022-06-17 10:16:30 -0300

    initial structure and custom ckeditor

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '570a9462620e9449d89c29dd460da502'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f637e746-3730-4fff-ad20-200ade592dca'
+};
